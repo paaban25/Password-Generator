26b2910d090bd0e9ad7176ed8ae960780ede8e93
paaban25 <122373317+paaban25@users.noreply.github.com>
2023-03-24 12:08:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const upperSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerSet="abcdefghijklmnopqrstuvwxyz";
 const numberSet="1234567890";
-const symbolSet="~!@#$%^&*()_+";
+const symbolSet="~!@#$%^&*()_+";                                        //Set of all posiible characters.
 
 const passBox=document.getElementById("pass-box");
 const totalChar=document.getElementById("total-char");
@@ -11,27 +11,27 @@ const numberInput=document.getElementById("numbers");
 const symbolInput=document.getElementById("symbols");
 
 const getRandomData = (dataSet)=>{
-    return dataSet[Math.floor(Math.random()*dataSet.length)];
+    return dataSet[Math.floor(Math.random()*dataSet.length)];           //Generating a random character from any "dataSet" passed as argument
 }
 
 const generatePassword = (password="")=>{
     if (upperInput.checked) {
-        password += getRandomData(upperSet);
+        password += getRandomData(upperSet);                            //Adding a random uppercase character if ITS checkbox is ticked.
     }
     if (lowerInput.checked) {
-        password += getRandomData(lowerSet);
+        password += getRandomData(lowerSet);                            //Adding a random lowercase character if ITS checkbox is ticked.
     }
     if (numberInput.checked) {
-        password += getRandomData(numberSet);
+        password += getRandomData(numberSet);                           //Adding a random number if ITS checkbox is ticked.
     }
     if (symbolInput.checked) {
-        password += getRandomData(symbolSet);
+        password += getRandomData(symbolSet);                           //Adding a random symbol if ITS checkbox is ticked.
     }
     if (password.length < totalChar.value){
-        return generatePassword(password);
+        return generatePassword(password);                              
     }
 
-    passBox.innerText= truncateString(password,totalChar.value);
+    passBox.innerText= truncateString(password,totalChar.value);        //Truncate the string to length desired by user if it exceeds.
     
 
 }
@@ -41,11 +41,11 @@ generatePassword();
 document.getElementById("btn").addEventListener(
     "click",
     function(){
-        generatePassword();
+        generatePassword();                                             //Function to show the random number when "Generate" button is clicked.
     }
 )
 
-function truncateString(str,num){
+function truncateString(str,num){                                       //Function to truncate a string to a given length.
     if(str.length > num ){
         let subStr = str.substring(0,num);
         return subStr;
